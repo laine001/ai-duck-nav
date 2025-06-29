2abf4555e5eb558a8b6d36a98da8bd98f6c89a52
laine001 <40457081+laine001@users.noreply.github.com>
2025-06-29 13:07:33 +0800

    ☀️

--- src/components/FloatMenu/index.tsx
+++ src/components/FloatMenu/index.tsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUp, faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FloatMenu: React.FC = () => {
   const { i18n } = useTranslation();
 
-  const [isDarkMode] = useState(() => {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
     return (
       typeof window !== "undefined" &&
       window.localStorage.getItem("darkMode") === "true"
     );
   });
   useEffect(() => {
-    console.log(isDarkMode, "html");
     if (typeof document !== "undefined") {
       const html = document.documentElement;
       if (isDarkMode) {
@@ -31,10 +30,9 @@ const FloatMenu: React.FC = () => {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
-  // const toggleDarkMode = () => {
-  //   console.log(isDarkMode, "toggleDarkMode");
-  //   setIsDarkMode(!isDarkMode);
-  // };
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   const changeLanguage = () => {
     i18n.changeLanguage(i18n.language === "en" ? "zh" : "en");
@@ -46,7 +44,7 @@ const FloatMenu: React.FC = () => {
         <div
           tabIndex={0}
           role="button"
-          className="btn btn-circle btn-ghost bg-base-200"
+          className="btn btn-circle bg-base-200 dark:bg-gray-800 dark:text-white"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -65,52 +63,22 @@ const FloatMenu: React.FC = () => {
         </div>
         <ul
           tabIndex={0}
-          className="dropdown-content menu bg-base-100 rounded-box z-1 shadow-sm p-2 gap-2"
+          className="dropdown-content menu bg-base-100 rounded-box z-1 shadow-sm p-2 gap-2  dark:bg-indigo-200 dark:text-white"
         >
           <li>
-            <button onClick={scrollToTop} className="btn btn-circle btn-ghost">
+            <button onClick={scrollToTop} className="btn btn-circle">
               <FontAwesomeIcon className="text-red text-xl" icon={faArrowUp} />
             </button>
           </li>
-          {/* <li>
-            <button onClick={toggleDarkMode}>
-              <svg
-                className="swap-on h-7 w-7 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-              </svg>
+          <li>
+            <button className="btn btn-circle" onClick={toggleDarkMode}>
+              <FontAwesomeIcon className="text-red text-xl" icon={faCircleHalfStroke} />
             </button>
-          </li> */}
-          {/* <li>
-            <button
-              onClick={toggleDarkMode}
-              className="btn btn-circle btn-ghost"
-            >
-              <label className="swap swap-rotate">
-                <input type="checkbox" />
-                <svg
-                  className="swap-on h-7 w-7 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                </svg>
-                <svg
-                  className="swap-off h-7 w-7 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                </svg>
-              </label>
-            </button>
-          </li> */}
+          </li>
           <li>
             <button
               onClick={changeLanguage}
-              className="btn btn-circle btn-ghost"
+              className="btn btn-circle"
             >
               {/* 这里需要添加中英文切换的图标 */}
               <svg
